3d040be7ccbf7f48eab0c9b8228f768f26a726e5
Chirag-parmarr <chiragparmar13913@gmail.com>
2021-09-24 20:31:45 +0530

    homepage complete

--- thefrenchlaundry/src/components/Homepage.js
+++ thefrenchlaundry/src/components/Homepage.js
@@ -3,6 +3,7 @@ import { motion } from "framer-motion"
 import styled from 'styled-components'
 import burger from "../assets/burger.svg"
 import text from "../assets/text.svg"
+import mac from "../assets/mac.svg"
 
 function Homepage() {
 
@@ -10,16 +11,16 @@ function Homepage() {
     return (
 
         <>
-            <div className="row h-75">
+            <div className="row h-50 ">
                 <div className="col d-flex justify-content-center ">
-                    <div className="align-self-center " style={{  fontSize: 100, fontFamily: "'Luckiest Guy', cursive", color: "white" }} >
-                    <img className="ms-5 img-fluid" src={text} />
+                    <div className="align-self-center " >
+                        <img className="ms-5 img-fluid" src={text} />
                     </div>
                 </div>
-                <div className="col">
-                    <div className="postion-fit">
+                <div className="col d-flex justify-content-center">
+                    <div className="postion-fit align-self-center  ">
                         <motion.div
-                            animate={{ y: -10 }}
+                            animate={{ y: -15 }}
                             transition={{
                                 duration: 1,
                                 yoyo: Infinity
@@ -29,8 +30,22 @@ function Homepage() {
                         </motion.div>
                     </div>
                 </div>
-                <div className="col">
+                <div className="col  d-flex align-items-end">
+
                 </div>
+            </div>
+            <div className='row pt-5'>
+                <div className="col d-flex justify-content-end">
+                    <img className="img-fluid" src={mac} />
+                </div>
+                <div className="col d-flex justify-content-end">
+                    <Container  >
+                        Breakfast that’s
+                        freshly prepared<br />
+                        EVEN IF YOU ARE NOT!
+                    </Container>
+                </div>
+
             </div>
 
         </>
@@ -38,15 +53,19 @@ function Homepage() {
 }
 
 
-// const Container = styled.div`
-// background-image: url(${burger});;
 
 
-//   background-repeat: no-repeat;
-//  height: fit-content ;
-//  width:fit-content
+const Container = styled.div`
+background:#ffffff;
+color:#2C7E80;
+border-radius: 30px;
+padding: 20px;
+font-size: 30px;
+  background-repeat: no-repeat;
+ height: fit-content;
+ width:75%;
 
-// `;
+`;
 
 
 
